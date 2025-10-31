63dfd6484ce0dc212f47695aad43528ab3ace373
Waqhar <Waqhar.email@indivillage.co>
2025-10-31 16:37:21 +0530

    init comit

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float speed = 5.0f;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(Vector3.forward * speed * forwardInput);
+    }
+}
