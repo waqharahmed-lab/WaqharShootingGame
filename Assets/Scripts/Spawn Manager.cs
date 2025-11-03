0083a57a15183a8498451c4d580bcf5cc05aaba8
Waqhar <Waqhar.email@indivillage.co>
2025-11-03 15:28:48 +0530

    Added one variable to generate more enemies when it get destroyed

--- Assets/Scripts/Spawn Manager.cs
+++ Assets/Scripts/Spawn Manager.cs
@@ -4,25 +4,46 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    
-        public GameObject enemyPrefab;
-        private float spawnRange = 9.0f;
+    public GameObject enemyPrefab;
+    private float spawnRange = 9.0f;
+    private float zombieCount;
+    public int waveNumber;
 
-    // Start is called before the first frame update
     void Start()
     {
-        for(int i=0; i<3; i++)
-
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        // Spawn the first wave
+        SpawnEnemyWave(waveNumber);
     }
-        private Vector3 GenerateSpawnPosition()
+
+    void Update()
     {
-         float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        // Count how many zombies are currently active
+        zombieCount = FindObjectsOfType<zombieMove>().Length;
+
+        // If all zombies are destroyed, spawn a new wave
+        if (zombieCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+        }
+    }
+
+    void SpawnEnemyWave(int numberOfZombies)
+    {
+         // how many zombies per wave
+
+        for (int i = 0; i < numberOfZombies; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
         float spawnPosZ = Random.Range(-spawnRange, spawnRange);
 
         Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
         return randomPos;
-
-        
     }
-    }
+}
