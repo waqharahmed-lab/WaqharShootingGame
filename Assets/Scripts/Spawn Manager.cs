7c41f109e959d8172860ef0d98ce470edc38514e
Waqhar <Waqhar.email@indivillage.co>
2025-11-03 14:49:07 +0530

    Added for loop for genrate multiple enemies

--- Assets/Scripts/Spawn Manager.cs
+++ Assets/Scripts/Spawn Manager.cs
@@ -8,14 +8,21 @@ public class SpawnManager : MonoBehaviour
         public GameObject enemyPrefab;
         private float spawnRange = 9.0f;
 
-        // Start is called before the first frame update
-        void Start()
-        {
-            float spawnPosX = Random.Range(-spawnRange, spawnRange);
-            float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i=0; i<3; i++)
 
-            Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+    }
+        private Vector3 GenerateSpawnPosition()
+    {
+         float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
 
-            Instantiate(enemyPrefab, randomPos, enemyPrefab.transform.rotation);
-        }
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        return randomPos;
+
+        
+    }
     }
