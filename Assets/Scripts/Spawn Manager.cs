ca0e1c044314c942f4b35e7c7c169358aad923d6
Waqhar <Waqhar.email@indivillage.co>
2025-11-03 14:28:12 +0530

    Created one script spawnmanager and uses random.range function to generate random positions

---
+++ Assets/Scripts/Spawn Manager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    
+        public GameObject enemyPrefab;
+        private float spawnRange = 9.0f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            float spawnPosX = Random.Range(-spawnRange, spawnRange);
+            float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+
+            Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+
+            Instantiate(enemyPrefab, randomPos, enemyPrefab.transform.rotation);
+        }
+    }
