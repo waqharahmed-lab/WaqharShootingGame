b82ef486759884af3ebec3120c11c0f519b279ce
Waqhar <Waqhar.email@indivillage.co>
2025-11-07 11:57:22 +0530

    Added new Script to destroy the bullets in some range

---
+++ Assets/MyScripts/DestroyOutofBond.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBond : MonoBehaviour
+{
+    private float topBound = 30.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+
+        }
+        
+        
+    }
+}
