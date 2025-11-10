c00ad81741a6617109d21896a9ce98c234bc1055
Waqhar <Waqhar.email@indivillage.co>
2025-11-10 15:04:13 +0530

    Added new functionality cheatcode button whenver player have not enough coin he can use this

--- Assets/MyScripts/MainMenuUI.cs
+++ Assets/MyScripts/MainMenuUI.cs
@@ -4,25 +4,61 @@ using TMPro;  // ✅ Needed for TextMeshProUGUI
 
 public class MainMenuUI : MonoBehaviour
 {
-    public TextMeshProUGUI coinText;  
+    public TextMeshProUGUI coinText;
+    public TextMeshProUGUI messageText;
+    public int entryFee = 10;
+    private int totalCoins;
 
     void Start()
     {
-        UpdateCoinDisplay();  
+        UpdateCoinDisplay();
     }
 
     public void OnPlayButton()
     {
-        SceneManager.LoadScene(1);  
+        totalCoins = PlayerPrefs.GetInt("TotalCoins", 0);
+        if (totalCoins >= entryFee)
+        {
+            totalCoins -= entryFee;
+            PlayerPrefs.SetInt("TotalCoins", totalCoins);
+            PlayerPrefs.Save();
+
+            Debug.Log("entry fee paid and remaining coins:" + totalCoins);
+            UpdateCoinDisplay();
+
+            SceneManager.LoadScene(1);
+        }
+
+        else
+        {
+            if (messageText != null)
+            {
+                messageText.text = " Not enough coins!";
+            }
+        }
     }
 
 
     private void UpdateCoinDisplay()
     {
-        int totalCoins = PlayerPrefs.GetInt("TotalCoins", 0); 
+        int totalCoins = PlayerPrefs.GetInt("TotalCoins", 0);
         if (coinText != null)
         {
-            coinText.text = totalCoins.ToString();  
+            coinText.text = totalCoins.ToString();
         }
+    }
+    public void OnCheatAddCoins()
+    {
+        int totalCoins = PlayerPrefs.GetInt("TotalCoins", 0);
+        totalCoins += 100; 
+        PlayerPrefs.SetInt("TotalCoins", totalCoins);
+        PlayerPrefs.Save();
+
+        Debug.Log("Cheat used! Coins increased to: " + totalCoins);
+
+        UpdateCoinDisplay();
+
+        if (messageText != null)
+            messageText.text = "+100 coins added!";
     }
 }
